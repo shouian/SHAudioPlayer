7f694f13d66fefec1f3c187aed82def62386f7e3
shouian <amark9@hotmail.com>
2013-09-07 23:03:45 +0800

    Initial Commit

---
+++ SHAudioPlayer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SHAudioPlayer
+//
+//  Created by shouian on 13/9/7.
+//  Copyright (c) 2013年 shouian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SHAudioPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SHAudioPlayer
+//
+//  Created by shouian on 13/9/7.
+//  Copyright (c) 2013年 shouian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
